75dda8445e2b4ee0cd9debc25588ca6d12d60118
Luca Bianchini <luca.bianchini@fermi.mo.it>
2026-01-19 12:59:48 +0100

    Start exercise

---
+++ Mqtt e Web Socket/publishers_sensors/pub_humidity.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import time
+import random
+import json
+import paho.mqtt.client as mqtt
+
+#BROKER = "test.mosquitto.org"
+BROKER = "mqtt.ssh.edu.it"  #broker scolastico
+TOPIC = "sensor/humidity"
+
+client = mqtt.Client()
+client.connect(BROKER, 1883)
+
+print("Sensore simulato avviato")
+
+while True:
+    value = round(random.uniform(18, 30), 2)
+
+    payload = {
+        "sensor": "humidity",
+        "value": value,
+        "unit": "C",#cambiala
+        "timestamp": datetime.now().strftime("%H:%M:%S")
+    }
+
+    client.publish(TOPIC, json.dumps(payload))
+    print("Pubblicato:", payload)
+
+    time.sleep(1)
---
+++ Mqtt e Web Socket/publishers_sensors/pub_temperature.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import time
+import random
+import json
+import paho.mqtt.client as mqtt
+
+#BROKER = "test.mosquitto.org"
+BROKER = "mqtt.ssh.edu.it"  #broker scolastico
+TOPIC = "sensor/temperature"
+
+client = mqtt.Client()
+client.connect(BROKER, 1883)
+
+print("Sensore simulato avviato")
+
+while True:
+    value = round(random.uniform(18, 30), 2)
+
+    payload = {
+        "sensor": "temperature",
+        "value": value,
+        "unit": "C",
+        "timestamp": datetime.now().strftime("%H:%M:%S")
+    }
+
+    client.publish(TOPIC, json.dumps(payload))
+    print("Pubblicato:", payload)
+
+    time.sleep(1)
